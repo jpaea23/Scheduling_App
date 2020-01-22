cba34b7fd81032a362a1f6f780ca97944f022270
John Paea <jpaea23@gmail.com>
2020-01-23 10:10:30 +1100

    Feature Login Frontend Initial Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleSubmit = event => {
+    axios
+          .post("/api/login/", this.state)
+          .then(res => console.log(res.data))
+          .catch(err => console.log(err));
+    event.preventDefault();
+  };
+
+  onChangeHandler = event => {
+    this.setState({ [event.target.name]: event.target.value})
+  }
+
+  render(){
+    const { username, password } = this.state
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-row">
+            <label>Username: </label>
+            <input type="text" value={username} name="username" maxLength="100" onChange={this.onChangeHandler}/>
+          </div>
+          <div className="form-row">
+            <label>Password: </label>
+            <input type="password" value={password} name="password" maxLength="100" onChange={this.onChangeHandler}/>
+          </div>
+          <div className="submit-row">
+            <input type="submit" value="Log In" />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
