b172d201054071300cee9bff1876f3bdc9c83447
john <jpaea23@gmail.com>
2020-05-07 22:51:41 +1000

    #5 Function bug fix & Code refactoring

--- frontend/src/containers/Calendar/Calendar.test.js
+++ frontend/src/containers/Calendar/Calendar.test.js
@@ -20,7 +20,15 @@ const jobObjFound = {
       'jobId': 4,
       'clientId': 1,
     },
-    '15': {
+    '9': {
+      'jobId': 4,
+      'clientId': 1,
+    },
+    '14': {
+      'jobId': 7,
+      'clientId': 1,
+    },
+    '16': {
       'jobId': 7,
       'clientId': 1,
     },
@@ -35,39 +43,25 @@ const jobObjFound = {
 test('GenerateTimeslotArr', () => {
   const arr = calcAvailTimeSlot(jobObjFound, selectDateFill);
 
-  // return type is array
-  expect(arr).toEqual(expect.any(Array));
   // return match array
   expect(arr).toEqual([
-    {'7': {'jobId': 4, 'clientId': 1}}, 9, 10, 11, 12, 13, 14, {'15': {'jobId': 7, 'clientId': 1}}, 17,
+    {'7': {'jobId': 4, 'clientId': 1}},
+    {'9': {'jobId': 4, 'clientId': 1}},
+    11, 12, 13,
+    {'14': {'jobId': 7, 'clientId': 1}},
+    {'16': {'jobId': 7, 'clientId': 1}},
   ]);
-  // return obj match
-  expect(arr).toMatchObject([
-    {'7': {'clientId': 1}}, 9, 10, 11, 12, 13, 14, {'15': {'jobId': 7}}, 17,
-  ]);
-  // check timeslots
-  const timeNotSub1 = [7, 8, 15, 16];
-  const timeNotEdge = [6, 18];
-  expect(arr).toEqual(expect.arrayContaining([9, 17]));
-  expect(arr).toEqual(expect.not.arrayContaining(timeNotSub1));
-  expect(arr).toEqual(expect.not.arrayContaining(timeNotEdge));
+
+  expect(arr).toEqual(expect.arrayContaining([11, 12, 13]));
+  expect(arr).toEqual(expect.not.arrayContaining([7, 8, 14, 15, 16, 15]));
+  expect(arr).toEqual(expect.not.arrayContaining([6, 18]));
 });
 
 test('EmptyTimeslotArr', () => {
   const arr = calcAvailTimeSlot(jobObjEmpty, selectDateEmpty);
 
-  // return type is array
-  expect(arr).toEqual(expect.any(Array));
-  // return obj in array
-  expect(arr).toEqual(
-      expect.not.arrayContaining([
-        expect.any(Object),
-      ]),
-  );
   // check timeslots
   // standard expected day timeslot
-  const standEmptySubSet = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
-  const timeNotEdge = [6, 18];
-  expect(arr).toEqual(standEmptySubSet);
-  expect(arr).toEqual(expect.not.arrayContaining(timeNotEdge));
+  expect(arr).toEqual([7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]);
+  expect(arr).toEqual(expect.not.arrayContaining([6, 18]));
 });
--- frontend/src/containers/Calendar/Index.js
+++ frontend/src/containers/Calendar/Index.js
@@ -2,14 +2,15 @@ import * as TimeConst from '../../config/TimeSlotConst';
 
 export const calcAvailTimeSlot = (jobs, dateSelected) => {
   const timeslot = [];
-  let start = 7;
   const duration = TimeConst.DEFAULT_DURATION;
+  let start = 7;
 
   while (start < 18) {
     if (start in jobs[dateSelected]) {
       const jobTime = start+'';
       timeslot.push({[jobTime]: jobs[dateSelected][jobTime]});
       start += duration;
+      continue;
     }
     timeslot.push(start);
     start++;
